b38f1c8bc4c561c2164bc6abaccdf9c2b1aed791
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2014-07-19 17:13:03 -0400

    Impelemented direct labelling method

--- tools/error_correct.py
+++ tools/error_correct.py
@@ -5,6 +5,7 @@ import string
 import os
 import argparse
 import pickle
+import cPickle 
 import sys
 import json
 # minc
@@ -18,29 +19,58 @@ from sklearn import svm
 from sklearn import neighbors
 from sklearn import ensemble
 from sklearn import tree
+from sklearn import cross_validation
+from sklearn import preprocessing
 
 def prepare_features(options, images, auto_seg, mask):
     
+    #if options.coord:
+    # add features dependant on coordinates
+    c=np.mgrid[ 0:images[0].shape[0] , 0:images[0].shape[1] , 0:images[0].shape[2]]
+    image_no=len(images)
+    # use with center at 0 and 1.0 at the edge, could have used preprocessing 
     if options.coord:
-        # add features dependant on coordinates
-        c=np.mgrid[0:images[0].shape[0] , 0:images[0].shape[1] , 0:images[0].shape[2]]
-        # use with center at 0 and 1.0 at the edge, could have used preprocessing 
         images.append( ( c[0]-images[0].shape[0]/2.0)/ (images[0].shape[0]/2.0) )
         images.append( ( c[1]-images[0].shape[1]/2.0)/ (images[0].shape[1]/2.0) )
         images.append( ( c[2]-images[0].shape[2]/2.0)/ (images[0].shape[1]/2.0) )
-        
-    images.append( auto_seg ) # add auto labelling as a feature
-    # TODO add more features here
-
+    
+    aa=auto_seg-0.5
+    
+    # add auto labelling as a feature
+    images.append( aa ) 
+    
+    # add apparance and context images
+    for x in range(-1,2):
+        for y in range (-1,2):
+            for z in range(-1,2):
+                if x!=0 or y!=0 or z!=0:
+                    images.append( np.roll( np.roll( np.roll( images[0], shift=x, axis=0 ), shift=y, axis=1), shift=z, axis=2 ) )
+                    # add more context
+                    images.append( np.roll( np.roll( np.roll( aa,        shift=x, axis=0 ), shift=y, axis=1), shift=z, axis=2 ) )
+    
+    app_features=len(images)-image_no-3 # 3 spatial features
+    
+    # add joint features
+    if options.joint and options.coord:
+        for i in range(app_features):
+            # multiply apparance features by coordinate features 
+            images.append( images[0] * images[i+image_no+3] ) 
+            for j in range(3):
+                # multiply apparance features by coordinate features 
+                images.append( images[j+image_no] * images[i+image_no+3] ) 
+    
     # extract only what's needed
     return [  i[ mask>0 ] for i in images ] 
-    
+
+
 def convert_image_list(images):
     s=[]
     for (i,k) in enumerate(images):
         s.append(np.column_stack( tuple( np.ravel( j ) for j in k ) ) )
         print s[-1].shape
+    
     return np.vstack( tuple( i for i in s ) )
+
 
 def parse_options():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -73,12 +103,24 @@ def parse_options():
                     dest="debug",
                     default=False,
                     help='Print debugging information' )
+                    
+    parser.add_argument('--dump', 
+                    action="store_true",
+                    dest="dump",
+                    default=False,
+                    help='Dump first sample features (for debugging)' )
     
     parser.add_argument('--coord', 
                     action="store_true",
                     dest="coord",
                     default=False,
                     help='Use image coordinates as additional features' )
+                    
+    parser.add_argument('--joint', 
+                    action="store_true",
+                    dest="joint",
+                    default=False,
+                    help='Produce joint features between appearance and coordinate' )
     
     parser.add_argument('--random', 
                     type=int,
@@ -97,6 +139,7 @@ def parse_options():
     options = parser.parse_args()
     
     return options
+
 
 if __name__ == "__main__":
     history=minc.format_history(sys.argv)
@@ -123,19 +166,16 @@ if __name__ == "__main__":
             ground=minc.Label( inp[-2] ).data
             auto=minc.Label(   inp[-1] ).data
             
-            images=[ minc.Image(k).data for k in inp[0:-3] ]
+            images=[ preprocessing.scale(minc.Image(k).data) for k in inp[0:-3] ]
             
             training_images.append( prepare_features( options, images, auto, mask ) )
-            training_err.append( np.logical_xor( ground[mask>0], auto[mask>0] ) )
-            
-            print "....{}".format(i)
-            
-            if i == 0:
-                print "Dumping images..."
+            #training_err.append( np.logical_xor( ground[mask>0], auto[mask>0] ) )
+            training_err.append( ground[mask>0] ) # perform direct learning right now
+                        
+            if i == 0 and options.dump:
+                print "Dumping feature images..."
                 for (j,k) in enumerate( training_images[-1] ):
                     test=np.zeros_like( images[0] )
-                    #print test.shape
-                    #print k.shape
                     test[ mask>0 ]=k
                     out=minc.Image( data=test )
                     out.save( name="dump_{}.mnc".format(j), imitate=inp[0] )
@@ -166,12 +206,17 @@ if __name__ == "__main__":
         else:
             clf = svm.LinearSVC()
         
-        clf.fit(training_X,training_Y)
+        #scores = cross_validation.cross_val_score(clf, training_X, training_Y)
+        #print scores
         
-        if options.debug: print(clf)
+        clf.fit( training_X, training_Y )
+        
+        #print(clf.score(training_X,training_Y))
+        
+        if options.debug: print( clf )
         
         with open(options.save,'wb') as f:
-            pickle.dump(clf, f)
+            cPickle.dump(clf, f, -1)
                 
     elif options.load  is not None and \
          options.input is not None and \
@@ -179,16 +224,16 @@ if __name__ == "__main__":
          options.mask  is not None and \
          options.output is not None:
         
-        if options.debug: print("Runnin error-correction...")
+        if options.debug: print( "Runnin error-correction..." )
         
         with open(options.load, 'rb') as f:
-            clf = pickle.load(f)
+            clf = cPickle.load(f)
         
-        if options.debug: print(clf)
+        if options.debug: print( clf )
         
-        if options.debug: print("Loading input images...")
+        if options.debug: print( "Loading input images..." )
         
-        images=[ minc.Image( i ).data for i in options.image ]
+        images=[ preprocessing.scale( minc.Image( i ).data ) for i in options.image ]
         
         mask=minc.Label( options.mask ).data
         auto=minc.Label( options.input ).data
@@ -201,11 +246,11 @@ if __name__ == "__main__":
         
         if options.debug: print("Running classifier...")
         
-        out_cls[mask>0] = clf.predict( test_x ) # np.logical_xor(clf.predict( test_x ), auto[mask>0] )
+        out_cls[ mask>0 ] = clf.predict( test_x ) # np.logical_xor(clf.predict( test_x ), auto[mask>0] )
     
         if options.debug: print("Saving output...")
         
         out=minc.Label( data=out_cls )
         out.save(name=options.output, imitate=options.input, history=history)
     else:
-        print "Error in arguments"
+        print "Error in arguments, run with --help"
